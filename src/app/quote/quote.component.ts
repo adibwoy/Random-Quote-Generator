4abad416116939f2bfda452019535090c6529a5c
Aditya Bhandari <adityab@vmware.com>
2017-03-19 19:20:11 -0700

    Final Commit
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { QuoteService } from "../quote.service";
 import { Quote } from "../model/quote";
 
+const categories: string[] = ["movies", "famous"];
+
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
@@ -13,12 +15,13 @@ export class QuoteComponent {
   private currentQuote: Quote;
 
   constructor(private quoteService: QuoteService) {
-    this.getNewQuote("movies");
+    this.getNewQuote();
   }
 
-  getNewQuote(category: string): void {
+  getNewQuote(): void {
+    let category: string = categories[Math.floor(Math.random()*categories.length)];
     this.quoteService.requestQuote(category).subscribe((response) => {
-      this.currentQuote = <Quote>response.json();
+      this.currentQuote = response.json() as Quote;
     })
   }
 
