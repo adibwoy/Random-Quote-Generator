e59f665b8390093917ab7d9f0de6e35861cae66e
Aditya Bhandari <adityab@vmware.com>
2017-03-26 17:32:32 -0700

    Initial Commit
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

--- src/app/api/api-details.ts
+++
@@ -1,2 +0,0 @@
-export const MASHAPE_API_KEY: string = "OkL2NJf7c5mshW2XI9gqMZZbNt3Ep1WpQeljsn8r3PfeN95gNC";
-export const MASHAPE_API_URL: string = "https://andruxnet-random-famous-quotes.p.mashape.com/";
